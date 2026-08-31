9c2d1037aaca3337dbcb456ed10c73ea98c0bcf3
Sascha Petrik <dev@petrik.de>
2026-08-31 18:00:50 +0200

    Update toggleMute.rb

--- Casks/toggleMute.rb
+++ Casks/toggleMute.rb
@@ -1,6 +1,6 @@
 cask "togglemute" do
-  version "2.0"
-  sha256 "a17d1c4c63a6c952c80c4c3e467903a3793c4277d77f22aa921c3747ae337842"
+  version "2.1"
+  sha256 "eae73e4585cdf84e56da01aa5cfc6881685e885e6ce9909ace49a36bea6a261d"
   url "https://github.com/satrik/togglemute/releases/download/#{version}/toggleMute.dmg"
   name "toggleMute"
   desc "macOS Menu Bar App to mute/unmute the microphone"
